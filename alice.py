7d49ac7fc889868ed58ab4a8717e99f32bc4afde
Violeta-16 <ungureanuvioletamihaela@gmail.com>
2025-11-15 17:35:35 +0200

    created alice and bob

---
+++ alice.py
@@ -0,0 +1,41 @@
+import random
+import socket
+import struct
+
+N = 50 # number of bits
+
+def main():
+    x= [random.randint(0, 1) for _ in range(N)]
+    y= [random.randint(0, 1) for _ in range(N)]
+
+    # qserver = socket.create_connection(('localhost', 5001))
+    # for (bit, qbit) in zip(x, y):
+    #     qserver.sendall(struct.pack("!I",bit ))
+    #     qserver.sendall(struct.pack("!I",qbit ))
+    #
+    # qserver.close()
+
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    client_socket.bind(('localhost', 4567))
+    client_socket.listen()
+
+    conn, address = client_socket.accept()
+
+    bob_bases = []
+    for _ in range(N):
+        result = conn.recv(4)
+        result = struct.unpack('!I', result)[0]
+        bob_bases.append(result)
+
+    for qbit in y:
+        conn.sendall(struct.pack("!I", qbit ))
+
+    print("Alice's bases: ", y)
+    print("Bob's bases: ", bob_bases)
+
+    conn.close()
+    client_socket.close()
+
+if __name__ == "__main__":
+    main()
---
+++ bob.py
@@ -0,0 +1,33 @@
+import random
+import socket
+import struct
+
+N = 50
+
+def main():
+    y = [random.randint(0, 1) for _ in range(N)]
+
+    # qserver = socket.create_connection(('localhost', 5002))
+    # for qbit in y:
+    #     qserver.sendall(struct.pack("!I", qbit))
+    #
+    # qserver.close()
+
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect(('localhost', 4567))
+
+    for qbit in y:
+        client_socket.sendall(struct.pack("!I", qbit ))
+
+    alice_bases = []
+    for _ in range(N):
+        result = client_socket.recv(4)
+        result = struct.unpack('!I', result)[0]
+        alice_bases.append(result)
+
+    print("Alice's bases: ", alice_bases)
+    print("Bob's bases: ", y)
+
+    client_socket.close()
+if __name__ == "__main__":
+    main()
